1d067e707b78a524fa7bd231a7890c887e4fcb68
abdiel_249 <abdielorellana3@gmail.com>
2021-07-01 07:46:10 -0400

    delete comments in main class

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import monitor.Monitor;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("diagnostico");
         Monitor monitor = new Monitor();
         monitor.monitorear();
-        System.out.println("El resultado es: "+monitor.getResultado());
     }
 }
