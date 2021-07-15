421ab4d9df40390b30d84874049b1f5ecc80ced9
smog <abdielorellana3@gmail.com>
2021-07-14 20:40:25 -0400

    make AlertPanel component

--- src/cargarregistros/gui/AlertPanel.java
+++ src/cargarregistros/gui/AlertPanel.java
@@ -1,9 +1,25 @@
 package cargarregistros.gui;
 
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class AlertPanel extends JPanel {
-    private JLabel phase;
-    private JLabel Message;
 
+    private JLabel phase;
+    private JLabel message;
+
+    private AlertPanel(){
+        phase = new JLabel();
+        message = new JLabel();
+        add(phase);
+        add(message);
+    }
+
+    public void setPhase(String phaseName){
+        phase.setText(phaseName);
+    }
+
+    public void setMessage(String m){
+        message.setText(m);
+    }
 }
